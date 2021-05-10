43c08360c652ba7a7062860f53838ed7faff3bc0
Adam Tornhill <adam@adamtornhill.com>
2021-05-10 10:11:55 +0200

    Introduce deep nested complexity with a bumpy road for regression tests on a branch

--- cpp-example.cpp
+++ cpp-example.cpp
@@ -1,3 +1,24 @@
 int main() { }
 
-inf f(){return 0;}
+inf f(){
+
+  if (x) {
+  	if (y) {
+  		if (z) {
+  			if (xyz) {
+  				return 2;
+  			}
+  		}
+  	}
+  }
+
+  if (bumpy) {
+  	while (make_more_bumps) {
+  		if (cancel_it) {
+  			break;
+  		}
+  	}
+  }
+
+  return 0;
+}
