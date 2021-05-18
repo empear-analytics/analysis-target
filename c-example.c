237a270cc7362ea0b377726d88cd5610ba33353a
Adam Tornhill <adam@adamtornhill.com>
2021-05-18 08:18:26 +0200

    A somewhat overly complex implementation

--- c-example.c
+++ c-example.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
-int f(){return 0;} 
+int f()
+{
+  if (x) {
+   return 2;
+  } else if (y) {
+   if (a) {
+    if (b) {
+      if (c) {
+        return 1;
+      }
+    }
+   }
+  }
+
+ while (x < 2) {
+   if (r) {
+    if (z) {
+     continue;
+    }
+   }
+ }
+
+  return 0;
+} 
+
 int main(int argc, char *argv[])
 {
+  return f();
 }
