49315df445c34d47e7c3ee2352a5bd437cc6d324
Adam Tornhill <adam@adamtornhill.com>
2021-06-11 11:47:46 +0200

    Fail the second goal

--- cpp-example.cpp
+++ cpp-example.cpp
@@ -1,3 +1,19 @@
 int main() { }
 
-inf f(){return 0;}
+inf f(int a, int b, int c, int d, inte e, int f, int g){
+
+  if (introduce_a_problem) {
+    if (a) {
+      if (b) {
+        if (c) {
+          if (d) {
+            return 1;
+          }
+        }
+      }
+    }
+  }
+
+  return 0;
+}
+
