d7a33a920c4dee54bb6568eed30046257af7ff6b
Adam Tornhill <adam@adamtornhill.com>
2021-06-11 11:46:43 +0200

    Fail the first goal

--- c-example.c
+++ c-example.c
@@ -2,4 +2,17 @@
 int f(){return 0;} 
 int main(int argc, char *argv[])
 {
+  if (introduce_a_problem) {
+    if (a) {
+      if (b) {
+        if (c) {
+          if (d) {
+            return 1;
+          }
+        }
+      }
+    }
+  }
+
+  return 0;
 }
