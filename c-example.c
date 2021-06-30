ae40259227850d54cd516cbf0308fda01850810c
Adam Tornhill <adam@adamtornhill.com>
2021-06-30 14:14:20 +0200

    One more long function

--- c-example.c
+++ c-example.c
@@ -168,3 +168,167 @@ int main(int argc, char *argv[])
 	return a_long_fn(argv[0][0])
 }
 
+
+int another_long_fn(char b) {
+
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+
+
+
+	do_a();
+	do_a();
+	do_a();
+
+
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+
+
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+
+
+	if (a < b) {
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+	}
+
+	if (a > b) {
+		do_e();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+	}
+
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+
+
+
+	do_a();
+	do_a();
+	do_a();
+
+
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+
+
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+
+		do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+
+
+
+	do_a();
+	do_a();
+	do_a();
+
+
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+
+
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+
+	return 0;
+} 
+
+
