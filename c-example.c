60be4e5bc8eff243c561adbc55702b08cc8e272f
Adam Tornhill <adam@adamtornhill.com>
2021-06-30 13:46:48 +0200

    add a Large Method

--- c-example.c
+++ c-example.c
@@ -1,5 +1,69 @@
 #include <stdio.h>
-int f(){return 0;} 
+int f() {
+
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+	do_a();
+
+
+
+	do_a();
+	do_a();
+	do_a();
+
+
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+	do_b();
+
+
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+	do_c();
+
+
+	if (a < b) {
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+		do_d();	
+	}
+
+	return 0;
+} 
+
+
 int main(int argc, char *argv[])
 {
 }
