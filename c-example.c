4a6f963cfc956a20383e4049f597a24df8068499
Adam Tornhill <adam@adamtornhill.com>
2021-07-26 14:51:34 +0200

    Add more functions to pass the threshold

--- c-example.c
+++ c-example.c
@@ -3,6 +3,20 @@ int f(int a, int b, int c, int d, int e, int f)
 {
   return 0;
 }
+
+int g() {
+}
+
+int eg(void) {
+
+}
+
+int cg() {
+}
+
+char bx(int a) {
+  return convert(a);
+}
  
 int main(int argc, char *argv[])
 {
