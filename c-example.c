00e560cbfea93cc7bc0c392d291c823f5b7737f2
Adam Tornhill <adam@adamtornhill.com>
2021-07-26 14:54:14 +0200

    More complex

--- c-example.c
+++ c-example.c
@@ -4,18 +4,31 @@ int f(int a, int b, int c, int d, int e, int f)
   return 0;
 }
 
-int g() {
+int g(char a) {
 }
 
 int eg(void) {
-
+ return;
 }
 
-int cg() {
+int cg(MyType z, MyOtherType d) {
+  return;
 }
 
 char bx(int a) {
   return convert(a);
+}
+
+string_type convert_me(int a) {
+
+  return magic(a);
+}
+
+int fgh(char * txt) {
+
+ int a = strlent(txt);
+
+ return a;
 }
  
 int main(int argc, char *argv[])
