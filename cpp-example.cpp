d04988c3260e205c36403503069d638db6b3431b
Adam Tornhill <adam@adamtornhill.com>
2022-02-20 10:28:07 +0100

    Adapt parsed input to legacy API

--- cpp-example.cpp
+++ cpp-example.cpp
@@ -1,3 +1,28 @@
+std::string one_more(const char* a) {
+  return std::string(a);
+}
+
+std::string deeper(char* a, std::string type) {
+  return one_more(a);
+}
+
+void parse(char *a, std::string type) {
+  return deeper(a, type);
+}
+
+void without_strings(int a, int b) {
+  return;
+}
+
+int another(int a, double c) {
+  return a;
+}
+
+void finally(std::string a) {
+  print(a);
+}
+
 int main() { }
 
 inf f(){return 0;}
+
