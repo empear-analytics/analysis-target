8370a50e95e2145f6ce261403991a1cfe05b8704
Kalle Norrestam <kalle@norrestam.se>
2023-08-17 09:50:18 +0200

    Add even more code duplication

--- python-example.py
+++ python-example.py
@@ -655,6 +655,34 @@ def _send_receipt_email3(user: User, transaction: DbTransaction, email: str = No
         return False
 
 
+def _send_receipt_email4(user: User, transaction: DbTransaction, email: str = None) -> bool:
+    """Send an e-mail with the a receipt for the transaction to the user.
+        Return true if email address found.
+    """
+    if not email:
+        email = user.receipt_email
+    if email:
+        locale = user.locale
+        if locale is None:
+            locale = shared.config.default_locale
+        logger.debug("Send receipt in %s to %s", locale, user.receipt_email)
+        try:
+            t = Template(name='receipt_email', locale=locale, global_variables=shared.jinja_globals)
+            template_env = {'transaction': transaction}
+            subject = t.render(template_env, section='subject')
+            message = t.render(template_env, section='body')
+            content_type = t.content_type
+        except TemplateNotFound:
+            subject = "Receipt from BobCat"
+            message = f"Receipt for transaction: {transaction.id}"
+            content_type = 'text/plain'
+        services.email_sender.send(email_from=None, email_to=email, subject=subject, message=message,
+                                   content_type=content_type)
+        return True
+    else:
+        return False
+
+
 def _remove_id_from_optional_set(id: str, id_set: Optional[Set[str]]):
     """Remove id from set. Return True if removed or no set."""
     if id_set is None:
