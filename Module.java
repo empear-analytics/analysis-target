c048e283bbc595574d78fb7daba24b2d3fab1bf0
Rok Lenarcic <rok.lenarcic@gmail.com>
2024-04-16 11:24:30 +0200

    TEST

--- Module.java
+++ Module.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+public class ComplexMethod{public void postItem(Item a)throws ValidationException{if(a.isNew()){if(a.getX()!=null&&a.getY()!=null&&a.getZ()!=null){post(a)}else{throw new ValidationException("incomplete new object")}}else{if(a.getX()<10&&a.getY()>25&&a.getZ()>0){post(a)}else{throw new ValidationException("invalid update")}}}}
 
 package com.google.inject.util;
 
