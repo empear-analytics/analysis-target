9522223819b15b9b2c0b2f8412a65eec665c18d8
Adam Tornhill <adam@adamtornhill.com>
2026-05-21 13:05:28 +0200

    Adding support important check

--- csharp-example.cs
+++ csharp-example.cs
@@ -2,5 +2,9 @@ class TestClass
 {
     static void Main(string[] args)
     {
+        if (args.Length >= 2 && args[0] == "cli" && args[1] == "test-run")
+        {
+            System.Console.WriteLine("Running tests");
+        }
     }
 }
